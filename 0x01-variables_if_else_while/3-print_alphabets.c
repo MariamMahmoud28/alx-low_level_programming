d03369f1b86438ec357fbbfc662cc817bf6505b1
MariamMahmoud28 <mm0376722@gmail.com>
2023-08-14 12:45:16 -0700

    l

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,25 @@
-#include <stdio.h> /** * main - entry point * * Description: Write a program that prints the alphabet * * Return: always 0 (success) */ int main(void) { char l = 'a'; char L = 'A'; while (l <= 'z') { putchar(l); l++; } while (L <= 'Z') { putchar(L); L++; } putchar('\n'); return (0);
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all alphbet letters exceot q and e
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
