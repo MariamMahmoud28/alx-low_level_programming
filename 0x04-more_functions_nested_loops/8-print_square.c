d02bf7b40383849623ca8cf1a0b0eb072eff8437
MariamMahmoud28 <mm0376722@gmail.com>
2023-08-17 17:13:34 -0700

    l

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,8 +14,9 @@ void print_square(int size)
 
 	for (x = 1; x <= size; x++)
 	{
-		for (y = 1; y <= size; y++)
-			_putchar('#');
+		for	(y = 1; y <= size; y++)
+		{	_putchar('#');
 		_putchar('\n');
+	}
 	}
 }
