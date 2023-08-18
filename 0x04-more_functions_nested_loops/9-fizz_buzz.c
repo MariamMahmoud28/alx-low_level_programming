588d7d41019dc9b1ba7be7d277b8ea65f9b1db76
MariamMahmoud28 <mm0376722@gmail.com>
2023-08-17 18:37:24 -0700

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  *	numbers that are multiples of 3 print Fizz
  *	numbers that are multiples of 5 print Buzz
  *	numbers that are multiples of 3 and 5 print FizzBuzz
- *	each number and word to be sparated by space
+ *	each number and word to be separated by space
  *
  * Return: Always 0 (success)
 */
