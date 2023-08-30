9db9762ddd554cb0aeeee6aec2056585ff74da2c
MariamMahmoud28 <mm0376722@gmail.com>
2023-08-30 13:32:39 -0700

    test

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - value
+ * @x: base
+ * @y: pow
+ * Return: pointer to the null
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (_pow_recursion(x, y + 1) / x);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+
+	return (_pow_recursion(x, y - 1) * x);
+}
