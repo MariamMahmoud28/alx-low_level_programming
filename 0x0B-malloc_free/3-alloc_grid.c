bc10ed06402694f4794e8697cd1bbba3858ac93b
MariamMahmoud28 <mm0376722@gmail.com>
2023-09-04 12:48:42 -0700

    test

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * **alloc_gri - returns a pointer
+ * @width: pointer
+ * @height: pointer
+ * Return: value
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **t, f, m;
+
+	t = malloc(sizeof(*t) * height);
+
+	if (width <= 0 || height <= 0 || t == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (f = 0; f < height; f++)
+		{
+			t[f] = malloc(sizeof(**t) * width);
+			if (t[f] == 0)
+			{
+				/*free malloc*/
+				while (f--)
+					free(t[f]);
+				free(t);
+				return (NULL);
+			}
+			for (m = 0; m < width; m++)
+				t[f][m] = 0;
+		}
+	}
+	return (t);
+}
+
