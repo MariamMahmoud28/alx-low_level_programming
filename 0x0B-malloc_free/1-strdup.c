dc3548388b37a3c94b2c5a36b8f1a5a067a5cb3f
MariamMahmoud28 <mm0376722@gmail.com>
2023-09-04 10:03:51 -0700

    test

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strdup - returns a pointer to a newly
+ * @str: pointer
+ * Return: value
+*/
+
+char *_strdup(char *str)
+{
+	int f = 0, z = 0;
+	char *n;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[z] != '\0'; z++)
+		;
+
+	/*+1 end of string*/
+	n = malloc(z * sizeof(*str) + 1);
+
+	if (n == 0)
+		return (NULL);
+	else
+	{
+		for (; f < z; f++)
+			n[f] = str[f];
+	}
+	return (n);
+}
