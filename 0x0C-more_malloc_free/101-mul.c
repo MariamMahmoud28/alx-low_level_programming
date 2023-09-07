c56111981de1734b780ea0f9b27531e70d156290
MariamMahmoud28 <mm0376722@gmail.com>
2023-09-07 05:33:37 -0700

    test

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+/**
+ * _puts - function
+ * @str: pointer
+ * Return: value
+ */
+
+void _puts(char *str)
+{
+	int m = 0;
+
+	while (str[m])
+	{
+		_putchar(str[m]);
+		m++;
+	}
+
+}
+
+/**
+ * _atoi - string
+ * @s: pointer
+ * Return: integer
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, m;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (m = firstNum; s[m] >= 48 && s[m] <= 57; m++)
+	{
+		resp *= 10;
+		resp += (s[m] - 48);
+	}
+
+	return (sign * resp);
+}
+
+/**
+ * print_int - integer
+ * @n: int
+ * Return: value
+ */
+
+void print_int(unsigned long int n)
+{
+	unsigned long int divisor = 1, m, resp;
+
+	for (m = 0; n / divisor > 9; m++, divisor *= 10)
+	;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+
+}
+
+/**
+ * main - result
+ * @argc: pointer
+ * @argv: list
+ * Return: 0
+ */
+
+int main(char const argc, int *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error");
+		exit(98);
+	}
+	print_int(_atoi(argv[1] * _atoi(argv[2]));
+			_putchar('\n');
+
+			return (0);
+}
