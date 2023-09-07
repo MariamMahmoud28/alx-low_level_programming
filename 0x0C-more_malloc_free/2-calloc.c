6103f565db15f2cc38139f91c5f0de277515beee
MariamMahmoud28 <mm0376722@gmail.com>
2023-09-07 03:02:49 -0700

    test

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *_memset - memory
+ * @s: pointer
+ * @b: pointer
+ * @n: pointer
+ * Return: value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return (ptr);
+}
+
+/**
+ * *_calloc - function
+ * @nmemb: pointer
+ * @size: pointer
+ * Return: value
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *z;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	z = malloc(sizeof(int) * nmemb);
+
+	if (z == 0)
+		return (NULL);
+
+	_memset(z, 0, sizeof(int) * nmemb);
+
+	return (z);
+}
