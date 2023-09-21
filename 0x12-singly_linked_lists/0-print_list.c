cd345eb6b26c795b5a494eae61547adf28d9bcf4
MariamMahmoud28 <mm0376722@gmail.com>
2023-09-21 04:34:41 -0700

    l

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns string
+ * @s: string length
+ *
+ * Return: value
+ */
+int _strlen(char *s)
+{
+	int m = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		m++;
+	return (m);
+}
+
+/**
+ * print_list - function
+ * @h: string
+ *
+ * Return: value
+ */
+size_t print_list(const list_t *h)
+{
+	size_t m = 0;
+
+	while (m)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+	}
+	return (m);
+}
