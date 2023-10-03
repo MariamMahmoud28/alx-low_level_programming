7c0964f0adbc4555e5216e5e1ec711ff9c31459a
MariamMahmoud28 <mm0376722@gmail.com>
2023-10-02 17:08:41 -0700

    l

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,13 +19,19 @@ int main(int ac, char **av)
 	char buf[READ_BUF_SIZE];
 
 	if (ac != 3)
+	{
 		dprintf(STDERR_FILENO, USAGE), exit(97);
+	}
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
+	{
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+	}
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (to_fd == -1)
+	{
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+	}
 
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
